c91b1b0e558badf11fa4574a7c93b8240ece6d79
Ubuntu <Christophe.driesen@hotmail.com>
2019-10-16 09:48:31 +0000

    changed sutdent id

--- lab04_terraform/azure/variables.tf
+++ lab04_terraform/azure/variables.tf
@@ -1,6 +1,6 @@
 # replace the X with your student ID
 variable "studentID" {
-  default = "X"
+  default = "11"
 }
 
 variable "azure_machine_type" {
--- lab04_terraform/google/variables.tf
+++ lab04_terraform/google/variables.tf
@@ -1,6 +1,6 @@
 # replace the X with your student ID
 variable "studentID" {
-  default = "X"
+  default = "11"
 }
 
 variable "gcp_machine_type" {
